0c83b41e190bed3cae1f0c0de6db1a8fda507d31
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-07-13 12:13:34 -0700

    Pop

---
+++ op_pop.c
@@ -0,0 +1,22 @@
+include "monty.h"
+
+/**
+ * op_pop - remove the element at the top of the stack
+ * @stack_p: stack pointer
+ */
+void op_pop(stack_t **stack_p)
+{
+	stack_t *top = NULL;
+
+	if (!*stack_p)
+		pfailure("L%u: can't pop an empty stack\n", op_env.lineno);
+
+	if (*stack_p != (*stack_p)->prev)
+	{
+		top = (*stack_p)->prev;
+		(*stack_p)->next->prev = top;
+		top->next = (*stack_p)->next;
+	}
+	free(*stack_p);
+	*stack_p = top;
+}
